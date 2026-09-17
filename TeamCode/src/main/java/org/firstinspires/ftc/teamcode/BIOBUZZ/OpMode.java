b60b441a39360157d7df9a9192f2877da7d24d58
maker-minds <augustin.bol@outlook.de>
2026-09-17 10:30:23 +0200

    Create OpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BIOBUZZ/OpMode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.BIOBUZZ;
+
+public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
